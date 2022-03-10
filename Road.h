aa5f611090db7014a9066010a387f7fc448c4e5a
jason <Liujason2003@gmail.com>
2022-03-10 15:05:01 +0100

    Road

--- Road.h
+++ Road.h
@@ -1,8 +1,13 @@
 #ifndef TRAFFICSIMULATION2022_ROAD_H
 #define TRAFFICSIMULATION2022_ROAD_H
+#include <string>
 
+using namespace std;
 
 class Road {
+    unsigned int length;
+
+    string roadName;
 
 };
 
