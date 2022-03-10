1e61040532f40f8f5832be4cdfc817dbeffef8c5
OrfeoTerkuci <terkuciorfeo@gmail.com>
2022-03-10 15:17:27 +0100

    Added .gitignore file and presentation file
    Signed-off-by: OrfeoTerkuci <terkuciorfeo@gmail.com>

--- TrafficSimulation.cpp
+++ TrafficSimulation.cpp
@@ -1,4 +1,7 @@
 #include "TrafficSimulation.h"
+#include "Road.h"
+#include "TrafficLight.h"
+#include "Vehicle.h"
 #include "tinyxml/tinyxml.h"
 TrafficSimulation::TrafficSimulation(const string &filename) : filename(filename) {}
 
