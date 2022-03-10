2788c19a9cb7407483fb5ad7e7963aa83c696869
jason <Liujason2003@gmail.com>
2022-03-10 15:30:57 +0100

    Standard_Values

---
+++ Standard_Values.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jason on 10.03.22.
+//
+
+#include "Standard_Values.h"
---
+++ Standard_Values.h
@@ -0,0 +1,14 @@
+//
+// Created by jason on 10.03.22.
+//
+
+#ifndef TRAFFICSIMULATION2022_STANDARD_VALUES_H
+#define TRAFFICSIMULATION2022_STANDARD_VALUES_H
+
+
+class Standard_Values {
+
+};
+
+
+#endif //TRAFFICSIMULATION2022_STANDARD_VALUES_H
