a389994ac67449bc6abd0e143ae88049e5dd8280
jason <Liujason2003@gmail.com>
2022-03-10 14:28:08 +0100

    TrafficSimulationReader

--- TrafficSimulationReader.h
+++ TrafficSimulationReader.h
@@ -1,15 +1,22 @@
 #ifndef TRAFFICSIMULATION2022_TRAFFICSIMULATIONREADER_H
 #define TRAFFICSIMULATION2022_TRAFFICSIMULATIONREADER_H
 #include <string>
+#include <vector>
 #include <fstream>
 
 using namespace std;
 
 class Road;
 class TrafficLight;
-class
+class Vehicle;
+
 class TrafficSimulationReader {
     string filename;
+
+    vector<Road> roads;
+    vector<TrafficLight> trafficLights;
+    vector<Vehicle> vehicles;
+
 public:
     TrafficSimulationReader(const string &filename);
 
