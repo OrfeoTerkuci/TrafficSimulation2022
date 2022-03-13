795b95ccf39bc243c6a5a3996810c84a73ecd04c
jason <Liujason2003@gmail.com>
2022-03-13 12:21:36 +0100

    TrafficSimulation

--- TrafficSimulation.cpp
+++ TrafficSimulation.cpp
@@ -3,12 +3,34 @@
 #include "TrafficLight.h"
 #include "Vehicle.h"
 #include "tinyxml/tinyxml.h"
+#include <stdexcept>
 
 TrafficSimulation::TrafficSimulation(const string &filename) : filename(filename) {
-    /*TiXmlDocument doc;
-    if(!doc.LoadFile(this->filename.c_str())) {
-        cerr << doc.ErrorDesc() << endl;
-    }*/
+    TiXmlDocument doc;
+
+    // File readable detection
+    try{
+        if(!doc.LoadFile(this->filename.c_str())) {
+            std::cerr << doc.ErrorDesc() << std::endl;
+            throw 1;
+        }
+    }
+
+    catch(int error){
+        if(error == 1){
+            std::cout << "File not readable" << std::endl;
+        }
+    }
+
+    // Check if file has elements to parse
+    TiXmlElement* root = doc.FirstChildElement();
+
+    if(root == NULL) {
+        std::cerr << "Failed to load file: No root element." << std::endl;
+        doc.Clear();
+    }
+
+
 }
 
 TrafficSimulation::TrafficSimulation() {}
