10c6b6bda74c59e46b6fe59c0ff6ab6edd97f80b
jason <Liujason2003@gmail.com>
2022-03-13 12:37:24 +0100

    Standard_Values

--- Standard_Values.h
+++ Standard_Values.h
@@ -7,23 +7,24 @@
 
 
 // Values
-#define LENGTH              4.0
-#define MAX_SPEED           16.6
-#define MAX_ACCELERATION    1.44
-#define MAX_BRAKE_FACTOR    4.61
-#define MIN_FOLLOW_DISTANCE 4.0
-#define SIMULATION_TIME     0.0166
-#define SLOWING_DISTANCE    50.0
-#define STOPPING_DISTANCE   15.0
-#define SLOWING_FACTOR      0.4
+#define LENGTH                  4.0
+#define MAX_SPEED               16.6
+#define MAX_ACCELERATION        1.44
+#define MAX_BRAKE_FACTOR        4.61
+#define MIN_FOLLOW_DISTANCE     4.0
+#define SIMULATION_TIME         0.0166
+#define SLOWING_DISTANCE        50.0
+#define STOPPING_DISTANCE       15.0
+#define SLOWING_FACTOR          0.4
 
 // Strings
-#define NAAM                "naam"
-#define BAANU               "BAAN"
-#define BAANL               "baan"
-#define LENGTE              "lengte"
-#define VERKEERSLICHT       "VERKEERSLICHT"
-#define VOERTUIG            "VOERTUIG"
-#define POSITIE             "positie"
+#define NAAM                    "naam"
+#define BAANU                   "BAAN"
+#define BAANL                   "baan"
+#define LENGTE                  "lengte"
+#define VERKEERSLICHT           "VERKEERSLICHT"
+#define VOERTUIG                "VOERTUIG"
+#define POSITIE                 "positie"
+#define CYCLUS                  "cyclus"
 
 #endif //TRAFFICSIMULATION2022_STANDARD_VALUES_H
