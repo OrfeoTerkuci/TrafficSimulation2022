b72cd6604ea1fa4126f81827b7bf8a05272f436b
jason <Liujason2003@gmail.com>
2022-04-05 14:11:23 +0200

    Parser functions of xml

---
+++ ParserXML.cpp
@@ -0,0 +1,23 @@
+// custom headers
+#include "TrafficSimulation.h"
+#include "TrafficLight.h"
+#include "Road.h"
+#include "Vehicle.h"
+#include "VehicleGenerator.h"
+#include "tinyxml/tinyxml.h"
+
+// build in libs
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+TrafficSimulation parseTrafficSimulation(const string &filename){}
+
+bool parseRoad(){}
+
+bool parseVehicle(){}
+
+bool parseTrafficLight(){}
+
+bool parseVehicleGenerator(){}
