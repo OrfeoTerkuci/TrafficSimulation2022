0fb8498ef9983c75b77f43786ece16f813b697cd
jason <Liujason2003@gmail.com>
2022-05-04 19:21:55 +0200

    Contracten voor Crossroad 50% gedaan

--- CrossRoad.cpp
+++ CrossRoad.cpp
@@ -1,25 +1,29 @@
-//
-// Created by jason on 05/04/2022.
-//
-
 #include "CrossRoad.h"
+#include "DesignByContract.h"
+#include "Road.h"
 #include <cstdlib>
 
 using namespace std;
 
 CrossRoad::CrossRoad() : switchRoad(false){
     init = this;
+    ENSURE(this == init, "init is not equal to itself, when calling constructor");
 }
 
 void CrossRoad::addRoad(Road *road, int position) {
+    REQUIRE(this->properlyInitialized(), "Crossroad was not properly initialized when calling addRoad");
+    REQUIRE(road->properlyInitialized(), "Road is not properly initialized when calling addRoad");
+    REQUIRE(position >= 0 and (unsigned) position <= road->getLength(), "position was out of bounds when calling addRoad");
     roads[road] = position;
 }
 
 map<Road *, int> &CrossRoad::getRoads(){
+    REQUIRE(this->properlyInitialized(), "Crossroad was not properly initialized when calling getRoad");
     return roads;
 }
 
 void CrossRoad::setRoads(const map<Road *, int> &newRoads) {
+    REQUIRE(this->properlyInitialized(), "Crossroad was not properly initialized when calling setRoad");
     CrossRoad::roads = newRoads;
 }
 
@@ -32,6 +36,7 @@ bool randomBool(){
 }
 
 void CrossRoad::simulateCrossroad(bool random, int modulo_number, int time) {
+    REQUIRE(this->properlyInitialized(), "Crossroad was not properly initialized when calling simulateCrossroad");
     // determine to switchroad
     if (random) {
         switchRoad = randomBool();
@@ -46,7 +51,9 @@ bool CrossRoad::isSwitchRoad() const {
     return switchRoad;
 }
 
-CrossRoad::~CrossRoad() {}
+CrossRoad::~CrossRoad() {
+    REQUIRE(this->properlyInitialized(), "crossroad was not properly initialized when calling destructor");
+}
 
 bool CrossRoad::properlyInitialized() {
     return init == this;
